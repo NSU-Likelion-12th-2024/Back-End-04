5283b3d87c999efe0db2446991354bc54b03463c
SHINSUMIN <ssm000104@hanmail.net>
2024-03-27 21:00:07 +0900

    homework

---
+++ 신수민/homework1.java
@@ -0,0 +1,19 @@
+package week01Day2;
+
+import java.util.Scanner;
+
+public class homework1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("숫자 : " + num);
+
+        for (int i = num; i <num +1 ; i++) {
+            for (int j = 1; j <10; j++) {
+                System.out.println(i+" * "+j + " = " +i*j );
+
+            }
+
+        }
+    }
+}
---
+++ 신수민/homework2.java
@@ -0,0 +1,29 @@
+package week01Day2;
+
+import java.util.Scanner;
+
+public class homework2 {
+    public static void main(String[] args) {
+
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Q.다음 중 프로그래밍 언어가 아닌 것은?");
+            System.out.println("1. 자바");
+            System.out.println("2. 파이썬");
+            System.out.println("3. c언어");
+            System.out.println("3. 멋쟁이사자처럼");
+
+            int num = scanner.nextInt();
+            if (num == 4){
+                System.out.println("정답");
+                break;
+            }
+            else {
+                System.out.println("오답");
+            }
+        }
+
+
+
+    }
+}
