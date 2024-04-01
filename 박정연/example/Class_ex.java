d2ed153f69caf37075b151c9ca9d16b00a47a839
JYeon-p <j_yeon_p@naver.com>
2024-04-01 20:25:27 +0900

    오버로링_오버라이딩예제

--- 박정연/example/Class_ex.java
+++ 박정연/example/Class_ex.java
@@ -5,10 +5,20 @@ package example;
         boolean power;
         int channel;
 
+        //클래스 생성자
+        TV(){}
+        TV(String color, boolean power, int channel){
+            //this.color = 클래스 매개변수_String color
+            this.color =color;
+            this.power =power;
+            this.channel = channel;
+        }
+
         void Power(){power =! power;}
         void ChannelUp(){channel++;}
         void ChannelDown(){channel--;}
     }
+
 public class Class_ex {
     public static void main(String[] args){
         TV LGtv = new TV();
---
+++ 박정연/example/Extend_ex.java
@@ -0,0 +1,40 @@
+package example;
+//=========================오버로딩
+class OverLoading{
+    void cat(){
+        System.out.println("나는 냐옹이다옹");
+    }
+    void cat(String bark){
+        System.out.println("고양이는 "+bark+"라고 짖는다옹");
+    }
+    void cat(int num){
+        System.out.println("고양이는 "+num+"마리 있다옹");
+    }
+}
+
+//=========================오버라이딩
+class Phone{
+    void message(){System.out.println("일반 메세지입니다.");}
+}
+class SmartPhone extends Phone{
+    @Override
+    void message(){System.out.println("오버라이딩 된 메세지입니다.");}
+}
+
+public class Extend_ex {
+    public static void main(String[] args){
+        //=========================오버로딩 결과
+        OverLoading Cat = new OverLoading();
+        Cat.cat();
+        Cat.cat("나옹!!");
+        Cat.cat(3);
+
+        System.out.println();
+
+        //=========================오버라이딩 결과
+        Phone phone = new Phone();
+        SmartPhone smartPhone = new SmartPhone();
+        phone.message();
+        smartPhone.message();
+    }
+}
