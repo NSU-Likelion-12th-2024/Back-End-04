bf333a2d3d060956cde2bbb30cfccafec995e85f
JYeon-p <j_yeon_p@naver.com>
2024-04-03 19:32:46 +0900

    링크드리스트예제

--- 박정연/homework/HiddenData.java
+++ 박정연/homework/HiddenData.java
@@ -1,33 +1,35 @@
 /*
-
 package homework;
 //개인정보 중 일부를 비공개로 전환하는 프로그램
 //개인정보를 비공개로 전환하는 메소드_getHiddenDate()
 //하나의 메소드에서 모든 동작 처리
 //각 정보는 아래 위치부터 비공개 적용
 
+
 // Substring(), length(), repeat() 사용
 
 import javax.lang.model.element.Name;
 
-class Data{
+class Data {
     String Name;
     private String Id;
     String Phone;
 
-    public Data(){} //생성자
+    public Data() {
+    } //생성자
 
-   void getHiddenData(String input,int index){
-        Data[] d=new Data[input.length()];
+    void getHiddenData(String input, int index) {
+        Data[] d = new Data[input.length()]; //배열 생성
+        //배열 d
 
+    }
 }
-
 public class HiddenData {
     public static void main(String[] args){
-
+        //이름, 주민번호, 전화번호 입력
 
      }
-       //이름, 주민번호, 전화번호 입력
-}
-*/
 
+}
+
+*/
