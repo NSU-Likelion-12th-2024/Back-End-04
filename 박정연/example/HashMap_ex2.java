7bcf9bcfc709edf6695f9cd99d19cbc6a47f7485
JYeon-p <j_yeon_p@naver.com>
2024-04-03 19:58:51 +0900

    map예제

---
+++ 박정연/example/HashMap_ex2.java
@@ -0,0 +1,28 @@
+package example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMap_ex2 {
+    public static void main(String[] args){
+        Map<String,Integer> ageMap = new HashMap<>();
+
+        ageMap.put("Alice",30);
+        ageMap.put("Bob",25);
+        ageMap.put("Charlie",35);
+
+        int bobAge1 = ageMap.get("Bob");
+        System.out.println("Bob의 나이 : "+bobAge1);
+
+        boolean containsBob = ageMap.containsKey("Bob");
+        System.out.println("Bod 키가 맵에 포함되어 있는가? "+containsBob);
+
+        ageMap.remove("Charlie");
+        System.out.println("Charlie 요소 제거 후 : "+ageMap);
+
+        ageMap.replace("Bob",26);
+
+        int bobAge2 = ageMap.get("Bob");
+        System.out.println("Bob의 변경된 나이 : "+bobAge2);
+    }
+}
