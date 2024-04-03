1b4c0204ac19e4db4a6fde0066db26b4655b21ed
JYeon-p <j_yeon_p@naver.com>
2024-04-03 18:32:03 +0900

    배열예제

---
+++ 박정연/example/Array_ex.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Array_ex {
+    public static void main(String[] args){
+        String[] mbti={"ENFP","ESTJ","INTP"};
+
+        System.out.println(mbti.length);
+
+        for(int i=0;i<mbti.length;i++){
+            System.out.println(mbti[i]);
+        }
+    }
+}
---
+++ 박정연/homework/HiddenData.java
@@ -0,0 +1,37 @@
+/*
+package homework;
+//개인정보 중 일부를 비공개로 전환하는 프로그램
+//개인정보를 비공개로 전환하는 메소드_getHiddenDate()
+//하나의 메소드에서 모든 동작 처리
+//각 정보는 아래 위치부터 비공개 적용
+
+// Substring(), length(), repeat() 사용
+
+import javax.lang.model.element.Name;
+
+class Data{
+    String Name;
+    private String Id;
+    String Phone;
+
+   void getHiddenData(String name_get,int index){
+       this.Name=name_get;
+       String name_arr =new String(name_get);
+
+   }
+    void getHiddenData(String id_get,int index){
+       this.Id=id_get;
+    }
+    void getHiddenData(String phone_get, int index){
+       this.Phone=phone_get;
+    }
+
+}
+
+public class HiddenData {
+    public static void main(String[] args){
+       //이름, 주민번호, 전화번호 입력
+
+    }
+}
+*/
