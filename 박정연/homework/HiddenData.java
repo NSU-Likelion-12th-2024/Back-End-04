4c70c7d7b7750b010d2d0d808baa07333294c352
JYeon-p <j_yeon_p@naver.com>
2024-04-06 23:58:23 +0900

    2주차 과제_1

--- 박정연/homework/HiddenData.java
+++ 박정연/homework/HiddenData.java
@@ -1,35 +1,40 @@
-/*
 package homework;
 //개인정보 중 일부를 비공개로 전환하는 프로그램
 //개인정보를 비공개로 전환하는 메소드_getHiddenDate()
-//하나의 메소드에서 모든 동작 처리
-//각 정보는 아래 위치부터 비공개 적용
-
-
-// Substring(), length(), repeat() 사용
-
-import javax.lang.model.element.Name;
 
 class Data {
     String Name;
     private String Id;
-    String Phone;
+    protected String Phone;
 
-    public Data() {
-    } //생성자
+    Data() {} //생성자
+    public void inputId(String id){
+        this.Id=id;
+    }
 
-    void getHiddenData(String input, int index) {
-        Data[] d = new Data[input.length()]; //배열 생성
-        //배열 d
-
+    public static String getHiddenData(String input,int index) { //메서드 getHiddenData()
+        String str=input.substring(0,index);
+        String str2="*".repeat(input.length()-index);
+        String result=str.concat(str2);
+        return result;
     }
 }
+
 public class HiddenData {
     public static void main(String[] args){
-        //이름, 주민번호, 전화번호 입력
+        Data data = new Data();
 
-     }
+        String name= "박자바";
+        String id = "010720-1234567";
+        String phone = "010-1234-5678";
+        data.Name=name;
+        data.inputId(id);
+        data.Phone=phone;
 
+        //문자->*_getHiddenData()
+        System.out.println("이름 : "+data.getHiddenData(name,1));
+        System.out.println("주민등록번호 : "+data.getHiddenData(id,8));
+        System.out.println("전화번호 : "+data.getHiddenData(phone,9));
+    }
 }
 
-*/
