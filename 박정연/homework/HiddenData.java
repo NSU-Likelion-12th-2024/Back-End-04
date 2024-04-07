ba5cb26f4b241fb654d93b05195f6d6a2c712006
JYeon-p <j_yeon_p@naver.com>
2024-04-07 18:18:10 +0900

    2주차 과제_3

--- 박정연/homework/HiddenData.java
+++ 박정연/homework/HiddenData.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package homework;
 //개인정보 중 일부를 비공개로 전환하는 프로그램
 //개인정보를 비공개로 전환하는 메소드_getHiddenDate()
@@ -39,46 +38,4 @@ class Data {
         System.out.println("주민등록번호 : "+data.getHiddenData(id,8));
         System.out.println("전화번호 : "+data.getHiddenData(phone,9));
     }
-=======
-package homework;
-//개인정보 중 일부를 비공개로 전환하는 프로그램
-//개인정보를 비공개로 전환하는 메소드_getHiddenDate()
-
-class Data {
-    String Name;
-    private String Id;
-    protected String Phone;
-
-    Data() {} //생성자
-    public static String getHiddenData(String input, int index) { //메서드 getHiddenData()
-        String str=input.substring(0,index);
-        String str2="*".repeat(input.length()-index);
-        String result =str.concat(str2);
-        return result;
-    }
-
-    public void inputId(String id) {
-        this.Id = id;
-    }
-}
-
-    public class HiddenData {
-        public static void main(String[] args){
-
-        Data data = new Data();
-
-        String name= "박자바";
-        String id = "010720-1234567";
-        String phone = "010-1234-5678";
-
-        data.Name=name;
-        data.inputId(id);
-        data.Phone=phone;
-
-        //문자->*_getHiddenData()
-        System.out.println("이름 : "+data.getHiddenData(name,1));
-        System.out.println("주민등록번호 : "+data.getHiddenData(id,8));
-        System.out.println("전화번호 : "+data.getHiddenData(phone,9));
-    }
->>>>>>> 414f34db6823b990722e447466c506029084445a
 }
---
+++ 박정연/homework/OddNumber_List.java
@@ -0,0 +1,26 @@
+package homework;
+//값을 입력하고 홀수값만 출력
+//list 사용
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class OddNumber_List {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Integer> Number =new ArrayList<Integer>(); //ArrayList 생성
+
+        System.out.println("10개의 수를 입력하세요"); //값 입력
+        for (int i = 0; i < 10; i++) {
+            Number.add(input.nextInt());
+        }
+
+        System.out.print("\n홀수 : ");
+        for(int k=0;k<10;k++){ //홀수값 출력
+            if(1==Number.get(k)%2){
+                System.out.print(Number.get(k));
+                System.out.print(" ");
+            }
+        }
+    }
+}
